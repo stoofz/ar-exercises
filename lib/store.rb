95ab95349d07c2170f259648ae1959b98150788c
Anthony Chambers <ajfchambers@gmail.com>
2023-07-27 10:11:15 -0400

    Add error handling for apparel

--- lib/store.rb
+++ lib/store.rb
@@ -4,4 +4,12 @@ class Store < ActiveRecord::Base
   validates :annual_revenue, numericality: { 
     only_integer: true,
     greater_than: 0 } 
+  validates :must_carry, with: :must_carry
+
+  def must_carry
+    unless mens_apparel || womens_apparel
+      errors.add(:base, "Store must carry either Men's or Women's apparel")
+    end
+  end
+
 end
